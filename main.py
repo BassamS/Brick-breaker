ba8781406cef54e08d90bd0b1a3e5d4e4055bc0b
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-14 15:15:53 +0200

    testing

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ width, height = 800, 600
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Brick Breaker')
 
+FPS = 60
 
-def draw():
+
+def draw(win):
     win.fill('white')
     pygame.display.update()
 
 
 def main():
+    clock = pygame.time.Clock()
+
     run = True
     while run:
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(win)
+
     pygame.quit()
     quit()
 
