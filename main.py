44555ceb09304b89d57dd4b95c98f5fdb7bab219
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-14 15:08:06 +0200

    main def

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import pygame
 width, height = 800, 600
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Brick Breaker')
+
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get()
+        if event.type == pygame.QUIT:
+            run = False
+            break
