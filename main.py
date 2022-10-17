87d5bffe4bd33107374fedb92fb3972d48e616a6
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-17 13:41:05 +0200

    Paddle

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ pygame.display.set_caption('Brick Breaker')
 FPS = 60
 
 
+class Paddle:
+    VEL = 5
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+
 def draw(win):
     win.fill('white')
     pygame.display.update()
