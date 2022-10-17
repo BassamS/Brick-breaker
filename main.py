ad5822c2e2c420dae70852dde0f79259a602425a
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-17 14:36:57 +0200

    Ball

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 
-width, height = 800, 600
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Brick Breaker')
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Brick Breaker")
 
 FPS = 60
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 15
 
 
 class Paddle:
@@ -17,14 +21,25 @@ class Paddle:
         self.height = height
         self.color = color
 
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.color, (self.x, self.y, self.width, self.height))
 
-def draw(win):
-    win.fill('white')
+    def move(self, direction=1):
+        self.x = self.x + self.VEL * direction
+
+
+def draw(win, paddle):
+    win.fill("white")
+    paddle.draw(win)
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
+
+    paddle = Paddle(WIDTH/2 - PADDLE_WIDTH/2, HEIGHT -
+                    PADDLE_HEIGHT - 5, PADDLE_WIDTH, PADDLE_HEIGHT, 'black')
 
     run = True
     while run:
@@ -35,7 +50,14 @@ def main():
                 run = False
                 break
 
-        draw(win)
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and paddle.x - paddle.VEL >= 0:
+            paddle.move(-1)
+        if keys[pygame.K_RIGHT] and paddle.x + paddle.width + paddle.VEL <= WIDTH:
+            paddle.move(1)
+
+        draw(win, paddle)
 
     pygame.quit()
     quit()
